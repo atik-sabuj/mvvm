7e04a7c460e8b10b3b3a6a5758da0fd60dba72a9
Atikuzzaman Sabuj <sabuj235@gmail.com>
2022-12-28 22:34:04 +0600

    Initial commit

---
+++ lib/respository/home_repository.dart
@@ -0,0 +1,24 @@
+
+import 'package:mvvm/data/network/BaseApiServices.dart';
+import 'package:mvvm/data/network/NetworkApiService.dart';
+import 'package:mvvm/model/movies_model.dart';
+import 'package:mvvm/res/app_url.dart';
+
+class HomeRepository {
+
+  BaseApiServices _apiServices = NetworkApiService() ;
+
+  Future<MovieListModel> fetchMoviesList()async{
+
+    try{
+
+      dynamic response = await _apiServices.getGetApiResponse(AppUrl.moviesListEndPoint);
+      print(response);
+      return response = MovieListModel.fromJson(response);
+
+    }catch(e){
+      throw e ;
+    }
+  }
+
+}
